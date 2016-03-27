a42fe4df01864c163b2f505a259a0cd1b62ddb20
sita dou <sita dou>
2016-03-27 18:08:49 +0900

    number_format.php 完了 伴って、README.md を更新

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # elites_kadai
 
 [課題] PHP基礎編(1):prime_number.php
+[課題]数字の出力フォーマット:number_format.php
--- number_format.php
+++ number_format.php
@@ -5,22 +5,20 @@ $n = 4;
 
 if ($m > 0 && $n > 2) {
 
-    // n 桁分の0を各配列に格納
-    $number = array_fill (0, $n, "0");
-    print_r ($number);
+    // m の値を文字列置換して一文字ずつ配列へ格納
+    $str_n = strval($m);
+    $len_n = mb_strlen($str_n);
+    $number = array_fill (0, $n - $len_n, "0");
 
-    // m の値を一桁ずつ上書きして格納
-    $str_n = strval($n);
-    vardump($str_n);
-    for($i=0;$i<strlen($str_n);$i++){
-        $c = $str[$i];
-        array_push($number,$c);
+    print_r($number);
+    print_r($str_n);
+    print_r($len_n);
+
+    for ($j=0 ; $j < $len_n ; $j++) {
+        $number[] = mb_substr($str_n, $j, 1, "UTF-8");
     }
 
-    // 出力
-    echo $number;
     print_r ($number);
-
 
 
 } else {
